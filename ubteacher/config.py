9cc4325bc810210fe2038fc796de5bf88b9c8a97
xingzhi <ningzhi.yang@vim-technology.com>
2026-05-23 19:30:49 +0800

    fix: keep sup percent config type stable

--- ubteacher/config.py
+++ ubteacher/config.py
@@ -45,7 +45,7 @@ def add_ubteacher_config(cfg):
 
     # dataloader
     # supervision level
-    _C.DATALOADER.SUP_PERCENT = 100.0  # 5 = 5% dataset as labeled set
+    _C.DATALOADER.SUP_PERCENT = 100  # 5 = 5% dataset as labeled set
     _C.DATALOADER.RANDOM_DATA_SEED = 0  # random seed to read data
     _C.DATALOADER.RANDOM_DATA_SEED_PATH = "dataseed/COCO_supervision.txt"
 
